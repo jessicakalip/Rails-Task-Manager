acf9d1adec13434520b26bc24d2c5e4435fcf6ff
jessica kalip <jessicakalip@gmail.com>
2021-07-20 15:00:48 +0800

    Completed Rails Task Manager

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,13 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+// const confirm = () => {
+//   if (confirm("Would you like to delete this task?")) {
+//   txt = "Task deleted!";
+// } else {
+//   txt = "Action cancelled";
+// }
+// }
+
+// const delete = document.querySelector("#destroy_button")
+// delete.addEventListener("click", () => {confirm})
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # # READ
+  # get '/tasks', to: 'tasks#index', as: :tasks
+  # # Need to put tasks/new before tasks/:id
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # # CREATE
+  # post '/tasks', to: 'tasks#create'
+
+  # # UPDATE
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
